407e82dcd4d75a9429ec723806d4a2a02f26b6ac
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-03-29 15:48:08 -0600

    Agregue objetos a el Juego

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const canvas = document.getElementById("game");
 
-console.log(canvas);
+const ctx = canvas.getContext("2d");
+
+//RECTANGULO
+
+ctx.fillStyle = "purple";
+ctx.fillRect(260, 260, 30, 30);
+
+
+ctx.fillStyle = "green";
+
+ctx.fillRect(25,25,100,100);
+
+ctx.clearRect(45,45,60,60);
+
+ctx.strokeRect(300,50,50,50);
+
+ctx.beginPath();
+    ctx.moveTo(450,150);
+    ctx.lineTo(450, 50);
+    ctx.stroke();
+ctx.closePath();
